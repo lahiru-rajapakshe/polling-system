08d2cdac142763cf348250de42ea0a730eda18ee
LahiruRJ <sanlahiru720@gmail.com>
2022-06-09 16:13:57 +0530

    Add poll component for dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PollComponent } from './poll/poll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    PollComponent
   ],
   imports: [
     BrowserModule,
