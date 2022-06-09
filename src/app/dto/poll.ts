78cd52c189834cd4f1f069e4b340cfe175560af0
LahiruRJ <sanlahiru720@gmail.com>
2022-06-09 20:05:17 +0530

    Add constroctor parameters for poll class

--- src/app/dto/poll.ts
+++ src/app/dto/poll.ts
@@ -1,2 +1,9 @@
 export class Poll {
+  constructor(id:number,
+              title:string,
+              createBy:string,
+              upVote:number=0,
+              downVote:number=0){
+
+  }
 }
