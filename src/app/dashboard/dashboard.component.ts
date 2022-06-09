8decd7974c4d3959df4e8a36aafd38fde5acbfa0
LahiruRJ <sanlahiru720@gmail.com>
2022-06-09 20:11:47 +0530

    Implement createPolls method

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Poll} from "../dto/poll";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  polls:Array<number=[];
+  polls:Array<Poll>=[];
 
 
 
@@ -16,7 +17,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  createPolls(){
-    this.polls.push(1);
+  createPolls(title:string){
+    this.polls.push(new Poll(Math.random(),title,this.userService.getPrincipal()));
   }
 }
